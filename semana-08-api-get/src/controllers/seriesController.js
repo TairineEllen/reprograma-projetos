094d9cdf0c4d01a718768876f0961312999aa1cd
TairineEllen <tairine.ellen@gmail.com>
2020-09-30 11:39:42 -0300

    Alteração de funções para getAllSeries e getSerieByID

--- semana-08-api-get/src/controllers/seriesController.js
+++ semana-08-api-get/src/controllers/seriesController.js
@@ -1,16 +1,16 @@
 const series = require('../models/series.json');
 
-const getAll = (req, res) => {
+const getAllSeries = (req, res) => {
     res.send(series);
 };
 
-const getByID = (req, res) => {
+const getSerieByID = (req, res) => {
     const id = req.params.id;
     const foundID = series.find((serie) => serie.id == id);
     res.send(foundID);
 };
 
 module.exports = {
-    getAll,
-    getByID
+    getAllSeries,
+    getSerieByID
 };
